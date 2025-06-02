cef47b0e36628ea82020bab886e0c357759d433b
aotokpa <andrewokibeotokpa@gmail.com>
2025-06-02 02:13:40 +0100

    cameltosnake file

--- cameltosnake/main.go
+++ cameltosnake/main.go
@@ -1,19 +1,15 @@
 package main
 
-import (
-	"fmt"
-	"unicode"
-)
-
 // camelToSnake converts a camelCase or PascalCase string to snake_case
 func camelToSnake(s string) string {
 	var result []rune
 	for i, r := range s {
-		if unicode.IsUpper(r) {
+		if r >= 'A' && r <= 'Z' {
 			if i > 0 {
 				result = append(result, '_')
 			}
-			result = append(result, unicode.ToLower(r))
+			// Convert uppercase to lowercase
+			result = append(result, r+'a'-'A')
 		} else {
 			result = append(result, r)
 		}
@@ -22,8 +18,8 @@ func camelToSnake(s string) string {
 }
 
 func main() {
-	fmt.Println(camelToSnake("camelCase"))   // Output: camel_case
-	fmt.Println(camelToSnake("CamelCase"))   // Output: camel_case
-	fmt.Println(camelToSnake("ThisIsATest")) // Output: this_is_a_test
-	fmt.Println(camelToSnake("simpleTest"))  // Output: simple_test
+	println(camelToSnake("camelCase"))   // Output: camel_case
+	println(camelToSnake("CamelCase"))   // Output: camel_case
+	println(camelToSnake("ThisIsATest")) // Output: this_is_a_test
+	println(camelToSnake("simpleTest"))  // Output: simple_test
 }
