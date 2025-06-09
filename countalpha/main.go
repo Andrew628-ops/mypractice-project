043b05a1c74ee2eb126e633c401d2b48c699e163
aotokpa <andrewokibeotokpa@gmail.com>
2025-06-09 17:21:01 +0100

    countalpha file

---
+++ countalpha/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func CountAlpha(s string) int {
+	count := 0
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+		if (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') {
+			count++
+		}
+	}
+	return count
+}
+
+func main() {
+	fmt.Println(CountAlpha("Hello world"))
+	fmt.Println(CountAlpha("H e l l o"))
+	fmt.Println(CountAlpha("H1e2l3l4o"))
+}
